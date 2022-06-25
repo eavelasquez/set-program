521bca6e7b8ba23c1a3bf31ebefeb9192af846ba
Esteban Velasquez <eavc.30@gmail.com>
2022-06-25 12:11:29 -0500

    feat: add logic to methods of class SetLinkedList
    - add new constructor.
    - complete interface method logic.

--- src/setprogram/SetLinkedList.java
+++ src/setprogram/SetLinkedList.java
@@ -53,36 +53,111 @@ public class SetLinkedList implements Set {
     }
   }
 
+  /**
+   * Returns true if this set contains the specified element.
+   * @param e the element to be checked for containment in this set
+   * @return true if this set contains e
+   */
   @Override
   public boolean contains(int e) {
-    // TODO Auto-generated method stub
+    Node current = head;
+    while (current != null) {
+      if (current.item == e) {
+        return true;
+      }
+      current = current.next;
+    }
     return false;
   }
 
+  /**
+   * Adds an element to this set if it is not already present.
+   * @param e the element to be added to this set
+   */
   @Override
   public void add(int e) {
-    // TODO Auto-generated method stub
+    if (!contains(e)) {
+      head = new Node(e, head);
+    } else {
+      System.out.println("Element " + e + " already exists in the set.");
+    }
   }
 
+  /**
+   * Removes an element from this set if it is present.
+   * @param e the element to be removed from this set, if present
+   */
   @Override
   public void remove(int e) {
-    // TODO Auto-generated method stub
+    if (contains(e)) {
+      Node current = head;
+      Node previous = null;
+      while (current != null) {
+        if (current.item == e) {
+          if (previous == null) {
+            head = current.next;
+          } else {
+            previous.next = current.next;
+          }
+        } else {
+          previous = current;
+        }
+        current = current.next;
+      }
+    } else {
+      System.out.println("Element " + e + " does not exist in the set.");
+    }
   }
 
+  /**
+   * Returns a string representation of this set.
+   * @return a string representation of this set
+   */
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    Node current = head;
+    sb.append("{");
+    while (current != null) {
+      sb.append(current.item);
+      current = current.next;
+      if (current != null) {
+        sb.append(", ");
+      }
+    }
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Returns the size of this set.
+   * @return the number of elements in this set
+   */
   @Override
   public int size() {
-    // TODO Auto-generated method stub
-    return 0;
+    int count = 0;
+    Node current = head;
+    while (current != null) {
+      count++;
+      current = current.next;
+    }
+    return count;
   }
 
+  /**
+   * Returns true if this set is empty.
+   * @return true if this set contains no elements
+   */
   @Override
   public boolean isEmpty() {
-    // TODO Auto-generated method stub
-    return false;
+    return head == null;
   }
 
+  /**
+   * Removes all of the elements from this set.
+   */
   @Override
   public void clear() {
-    // TODO Auto-generated method stub
+    head = null;
   }
 }
