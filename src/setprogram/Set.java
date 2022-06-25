7d0b71791b6d1e3f7456b8c246ed90d80045f56c
Esteban Velasquez <eavc.30@gmail.com>
2022-06-25 17:19:21 -0500

    doc: add comments in interface Set

--- src/setprogram/Set.java
+++ src/setprogram/Set.java
@@ -1,5 +1,10 @@
 package setprogram;
 
+/**
+ * The {@code Set} interface contains the common methods of a set.
+ *
+ * @author eavelasquez
+ */
 public interface Set {
   public boolean contains(int e);
 
