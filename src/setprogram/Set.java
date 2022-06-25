de7bfcc15257784967faf61664bdd2f283951950
Esteban Velasquez <eavc.30@gmail.com>
2022-06-25 11:37:18 -0500

    feat: add Set interface
    - add interface set for common methods among classes.

---
+++ src/setprogram/Set.java
@@ -0,0 +1,17 @@
+package setprogram;
+
+public interface Set {
+  public boolean contains(int e);
+
+  public void add(int e);
+
+  public void remove(int e);
+
+  public String toString();
+
+  public int size();
+
+  public boolean isEmpty();
+
+  public void clear();
+}
